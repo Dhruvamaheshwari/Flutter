e7a2ca705c5eab43e6e15cd47eca7a62b75e5912
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-09-23 01:43:36 +0530

    refactor: clean up commented code and improve class definition

--- lib/practise.dart
+++ lib/practise.dart
@@ -3,11 +3,25 @@ import 'dart:io';
 void main() {
   print("my name is Dhruva Maheshwari");
 
-  stdout.write("enter your name: ");
+  // stdout.write("enter your name: ");
 
-  var name = stdin.readLineSync();
+  // var name = stdin.readLineSync();
 
-  print("your name is $name ");
+  // print("your name is $name ");
+
+  // create the object of the human class
+  var male = new Human("Dhruva");
+  male.printname();
 }
 
+// how to define class
+class Human {
+  var name;
+  Human(String name) {
+    this.name = name;
+  }
 
+  void printname() {
+    print(name);
+  }
+}
